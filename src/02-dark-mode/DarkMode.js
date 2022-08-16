bbc02bebb813ae0dfc907c1d649ace727e4a7947
Sarah Thompson <sscotthompson@gmail.com>
2022-08-16 15:27:22 -0400

    fixed

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -4,8 +4,8 @@ export default function DarkMode () {
   const [darkMode, setDarkMode] = useState(false)
   return (
     <div className={`page ${darkMode && 'dark-mode'}`}>
-      <button className='dark-mode-button' onCLick={() => setDarkMode(true)}>Dark Mode</button>
-      <button className='light-mode-button' onCLick={() => setDarkMode(false)}>Light Mode</button>
+      <button className='dark-mode-button' onClick={() => setDarkMode(true)}>Dark Mode</button>
+      <button className='light-mode-button' onClick={() => setDarkMode(false)}>Light Mode</button>
     </div>
   )
 }
