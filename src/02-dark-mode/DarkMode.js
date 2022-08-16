df92712374c68970cc3b9365fadc5de686130c5e
Sarah Thompson <sscotthompson@gmail.com>
2022-08-16 15:18:16 -0400

    got buttons running

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -3,9 +3,9 @@ import { useState } from "react";
 export default function DarkMode () {
   const [darkMode, setDarkMode] = useState(false)
   return (
-    <div className='page'>
-      <button className='dark-mode-button' onCLick={() =>setDarkMode(true)}>Dark Mode</button>
-      <button className='light-mode-button' onCLick={() =>setDarkMode(false)}>Light Mode</button>
+    <div className={`page ${darkMode && 'dark-mode'}`}>
+      <button className='dark-mode-button' onCLick={() => setDarkMode(true)}>Dark Mode</button>
+      <button className='light-mode-button' onCLick={() => setDarkMode(false)}>Light Mode</button>
     </div>
   )
 }
